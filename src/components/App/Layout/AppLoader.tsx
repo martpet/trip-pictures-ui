f01f16dfae748b519452d840a872b670e3ea90ec
Martin Petrov <martin_petrov@me.com>
2022-02-20 19:08:42 +0200

    Render AppLoader outside of root

--- src/components/App/Layout/AppLoader.tsx
+++ src/components/App/Layout/AppLoader.tsx
@@ -1,17 +1,28 @@
 import { View } from '@adobe/react-spectrum';
+import { createPortal } from 'react-dom';
 import { useSelector } from 'react-redux';
 
-import { LoadingOverlay } from '~/components';
+import { LoadingOverlay, Theme } from '~/components';
 import { selectIsAppLoading } from '~/slices';
 
 export function AppLoader() {
   const isLoading = useSelector(selectIsAppLoading);
+  const containerNode = document.getElementById('loader')!;
 
   if (!isLoading) return null;
 
-  return (
-    <View position="fixed" left="0" top="0" width="100vw" height="100vh" zIndex={9999}>
+  const element = (
+    <View
+      position="fixed"
+      left="static-size-0"
+      top="static-size-0"
+      width="100vw"
+      height="100vh"
+      zIndex={9999}
+    >
       <LoadingOverlay />
     </View>
   );
+
+  return createPortal(<Theme>{element}</Theme>, containerNode);
 }
--- src/components/LoadingOverlay.tsx
+++ src/components/LoadingOverlay.tsx
@@ -1,32 +1,23 @@
 import { Flex, View } from '@adobe/react-spectrum';
-import { ReactNode } from 'react';
 
 import { Spinner } from '~/components';
 
 export function LoadingOverlay() {
   return (
-    <>
-      <Overlay>
-        <View
-          height="100%"
-          UNSAFE_style={{
-            background: 'var(--spectrum-alias-background-color-modal-overlay)',
-          }}
-        />
-      </Overlay>
-      <Overlay>
-        <Flex height="100%" alignItems="center" justifyContent="center">
-          <Spinner />
-        </Flex>
-      </Overlay>
-    </>
-  );
-}
-
-function Overlay({ children }: { children: ReactNode }) {
-  return (
-    <View position="absolute" left="0" top="0" width="100%" height="100%" zIndex={9999}>
-      {children}
+    <View
+      position="absolute"
+      left="static-size-0"
+      top="static-size-0"
+      width="100%"
+      height="100%"
+      zIndex={9999}
+      UNSAFE_style={{
+        background: 'var(--spectrum-alias-background-color-modal-overlay)',
+      }}
+    >
+      <Flex height="100%" alignItems="center" justifyContent="center">
+        <Spinner />
+      </Flex>
     </View>
   );
 }
