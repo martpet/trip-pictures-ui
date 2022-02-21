06257261ac3eac7ea3e2d3ecf59ea79f28366933
Martin Petrov <martin_petrov@me.com>
2022-02-21 11:03:30 +0200

    Improve lazy loading of dialogs

---
+++ src/components/Toolbar/SettingsPopover.tsx
@@ -0,0 +1,29 @@
+import { ActionButton, Content, Dialog, DialogTrigger } from '@adobe/react-spectrum';
+import SettingsIcon from '@spectrum-icons/workflow/Settings';
+import { lazy, Suspense, useState } from 'react';
+import { useIntl } from 'react-intl';
+
+import { Loader } from '~/components';
+
+const settingsPromise = import('~/lazy/settings');
+const Settings = lazy(() => settingsPromise);
+
+export function SettingsPopover() {
+  const [isOpen, setOpen] = useState(false);
+  const { formatMessage } = useIntl();
+
+  return (
+    <DialogTrigger type="popover" isOpen={isOpen} onOpenChange={setOpen}>
+      <ActionButton isQuiet aria-label={formatMessage({ id: 'toolbar.button.settings' })}>
+        <SettingsIcon />
+      </ActionButton>
+      <Dialog size="L">
+        <Content>
+          <Suspense fallback={<Loader />}>
+            <Settings />
+          </Suspense>
+        </Content>
+      </Dialog>
+    </DialogTrigger>
+  );
+}
--- src/components/Toolbar/UploadDialogTrigger.tsx
+++ src/components/Toolbar/UploadDialogTrigger.tsx
@@ -3,30 +3,32 @@ import UploadIcon from '@spectrum-icons/workflow/UploadToCloudOutline';
 import { lazy, Suspense, useState } from 'react';
 import { useIntl } from 'react-intl';
 
+import { Loader } from '~/components';
 import { useIsMobile } from '~/hooks';
 
-const UploadDialog = lazy(() => import('~/lazy/upload'));
+const uploadDialogPromise = import('~/lazy/upload');
+const UploadDialog = lazy(() => uploadDialogPromise);
 
 export function UploadDialogTrigger() {
   const [isOpen, setOpen] = useState(false);
-  const { formatMessage } = useIntl();
   const isMobile = useIsMobile();
   const dialogType = isMobile ? 'fullscreenTakeover' : 'fullscreen';
+  const { formatMessage } = useIntl();
 
   const handlePress = () => setOpen(true);
 
   return (
-    <Suspense fallback={null}>
-      <DialogTrigger type={dialogType} isOpen={isOpen}>
-        <ActionButton
-          onPress={handlePress}
-          isQuiet
-          aria-label={formatMessage({ id: 'toolbar.button.upload' })}
-        >
-          <UploadIcon />
-        </ActionButton>
+    <DialogTrigger type={dialogType} isOpen={isOpen}>
+      <ActionButton
+        onPress={handlePress}
+        isQuiet
+        aria-label={formatMessage({ id: 'toolbar.button.upload' })}
+      >
+        <UploadIcon />
+      </ActionButton>
+      <Suspense fallback={<Loader />}>
         <UploadDialog isOpen={isOpen} setOpen={setOpen} />
-      </DialogTrigger>
-    </Suspense>
+      </Suspense>
+    </DialogTrigger>
   );
 }
