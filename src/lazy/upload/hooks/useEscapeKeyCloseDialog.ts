b2bb38d6930924a1094718ae1a9b261f27303fec
Martin Petrov <martin_petrov@me.com>
2022-02-21 11:47:04 +0200

    Fix upload dialog escape key close

--- src/lazy/upload/hooks/useEscapeKeyCloseDialog.ts
+++ src/lazy/upload/hooks/useEscapeKeyCloseDialog.ts
@@ -2,7 +2,7 @@ import { useContext, useEffect } from 'react';
 
 import { UploadContext } from '~/lazy/upload';
 
-export const useCloseOnEscapeKey = () => {
+export const useEscapeKeyCloseDialog = () => {
   const { closeDialog } = useContext(UploadContext);
 
   useEffect(() => {
