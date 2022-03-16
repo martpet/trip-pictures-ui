f1d0854679e18ec5b61dc03bf2f47353dafe34bb
Martin Petrov <martin_petrov@me.com>
2022-03-16 19:33:14 +0200

    Rename api folders

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,6 @@
-export * from './static/staticApi';
-export * from './strapi/strapiApi';
+export * from './static-api/staticApi';
+export * from './strapi-api/strapiApi';
 //
-export * from './static/endpoints/translations';
-export * from './strapi/endpoints/photo';
-export * from './strapi/endpoints/user';
+export * from './static-api/endpoints/translations';
+export * from './strapi-api/endpoints/photo';
+export * from './strapi-api/endpoints/user';
