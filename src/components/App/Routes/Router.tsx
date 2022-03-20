9d87c3d240980a06dde2f3fd43a87d42f9e5556a
Martin Petrov <martin_petrov@me.com>
2022-03-20 11:36:43 +0200

    Rename MapView to MainView

--- src/components/App/Routes/Router.tsx
+++ src/components/App/Routes/Router.tsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 
-import { MapView, NotFound } from '~/components';
+import { MainView, NotFound } from '~/components';
 import { paths } from '~/consts';
 
 export function Router() {
   return (
     <Routes>
-      <Route path={paths.viewport} element={<MapView />} />
-      <Route path={paths.home} element={<MapView />} />
+      <Route path={paths.home} element={<MainView />} />
+      <Route path={paths.viewport} element={<MainView />} />
       <Route path={paths.notFound} element={<NotFound />} />
     </Routes>
   );
