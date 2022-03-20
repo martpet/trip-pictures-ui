527d78b203dc433cef7ebefcdaa2671080b121af
Martin Petrov <martin_petrov@me.com>
2022-03-20 14:10:51 +0200

    Rename useUploads useS3Upload hooks

--- src/lazyload/upload/hooks/index.ts
+++ src/lazyload/upload/hooks/index.ts
@@ -3,5 +3,5 @@ export * from './useEscapeKey';
 export * from './useFilteredUploads';
 export * from './useOpenCloseDialog';
 export * from './useRotateImage';
-export * from './useS3Upload';
-export * from './useUploads';
+export * from './useUpload';
+export * from './useUploadsEntities';
