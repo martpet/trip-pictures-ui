1e5e4533b2a7f20abf38117474f428dca75704fc
Martin Petrov <martin_petrov@me.com>
2022-03-28 22:44:52 +0300

    Fix resizing map when toolbar position changed

--- src/components/Map/MapGL.tsx
+++ src/components/Map/MapGL.tsx
@@ -1,12 +1,17 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-import { ReactNode, useEffect, useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import { ReactNode, useEffect, useRef, useState } from 'react';
+import ReactMapGL, { MapRef } from 'react-map-gl';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { Layers } from '~/components';
 import { useDebounce } from '~/hooks';
-import { selectColorScheme, selectViewState, viewStateChanged } from '~/slices';
+import {
+  selectColorScheme,
+  selectToolbarPosition,
+  selectViewState,
+  viewStateChanged,
+} from '~/slices';
 import { setViewStateInUrl } from '~/utils';
 
 type Props = {
@@ -19,6 +24,8 @@ export function MapGL({ children }: Props) {
   const [viewState, setViewState] = useState(storedViewState);
   const debouncedViewState = useDebounce(viewState);
   const colorScheme = useSelector(selectColorScheme);
+  const toolbarPosition = useSelector(selectToolbarPosition);
+  const mapRef = useRef<MapRef>(null);
 
   useEffect(() => {
     if (viewState) {
@@ -27,10 +34,15 @@ export function MapGL({ children }: Props) {
     }
   }, [debouncedViewState]);
 
+  useEffect(() => {
+    mapRef.current?.resize();
+  }, [toolbarPosition]);
+
   if (!colorScheme) return null;
 
   return (
     <ReactMapGL
+      ref={mapRef}
       {...viewState}
       mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
       mapStyle={`mapbox://styles/mapbox/${colorScheme}-v10`}
